3d9c2b05f76d36aa925ee5758c05f0019c0b483b
JÉSSICA ALVES DO NASCIMENTO <JESSICA0865@digitalcollegesul.local>
2024-10-23 12:10:31 -0300

    falta

---
+++ index.js
@@ -0,0 +1,18 @@
+let numeroGerado = parseInt(Marth.random() * 100); //parseint serve para armazenar numeros inteiros
+console.log(numeroGerado);
+let tentativas = [];
+
+numeroGerado.oncharge = ()=> {
+    let numeroDigitado = Number (numero.value);
+    if (tentativas.length < 10){
+        if(tentativas. legth >0){
+            for (let i = 0; i < tentativas.length; i++){
+                if(tentativas[i] == numeroDigitado){
+                    
+                }
+            }
+        }
+    }
+}
+
+
